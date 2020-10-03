31c3bb1f40a760c6fcdd54d17b1bd8645c23281a
Gustavo Piá <piagja@gmail.com>
2020-10-03 16:29:49 -0300

    commit inicial do frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Nokia from './components/Nokia/Nokia'
+import './App.css'
+
+function App () {
+  return (
+    <div class='App'>
+      <Nokia />
+    </div>
+  )
+}
+
+export default App
