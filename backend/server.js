458480a843de8e074e2557a0111acaa1d77e8daf
Renato  Avellar <onizuka@Mac-mini.local>
2020-10-03 17:32:21 -0300

    conversor

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,88 @@ app.get("/", (req, res) =>{
     return res.send("hello");
 })
 
+app.post("/", (req, res) =>{
+    var result = '202220222033066080880777033'.split('0')
+var contagem =[]
+var frase =[]
+for(i=0;i<result.length;i++){
+    contagem.push(result[i].length)
+    for(j=i;j<contagem.length;j++){
+      if(result[i].startsWith(2)||result[i].startsWith('#2')){
+        if(result[i].length===1){
+          frase.push('a')
+        }else if(result[i].length===2){
+          frase.push('b')
+        }else if(result[i].length===3){
+          frase.push('c')}
+      }else if(result[i].startsWith(3)){
+        if(result[i].length===1){
+          frase.push('d')
+        }else if(result[i].length===2){
+          frase.push('e')
+        }else if(result[i].length===3){
+          frase.push('f')}
+      }else if(result[i].startsWith(4)){
+        if(result[i].length===1){
+          frase.push('g')
+        }else if(result[i].length===2){
+          frase.push('h')
+        }else if(result[i].length===3){
+          frase.push('i')
+        }
+      }else if(result[i].startsWith(5)){
+        if(result[i].length===1){
+          frase.push('j')
+        }else if(result[i].length===2){
+          frase.push('k')
+        }else if(result[i].length===3){
+          frase.push('l')
+        }
+      }else if(result[i].startsWith(6)){
+        if(result[i].length===1){
+          frase.push('m')
+        }else if(result[i].length===2){
+          frase.push('n')
+        }else if(result[i].length===3){
+          frase.push('o')
+        }
+      }else if(result[i].startsWith(7)){
+        if(result[i].length===1){
+          frase.push('p')
+        }else if(result[i].length===2){
+          frase.push('q')
+        }else if(result[i].length===3){
+          frase.push('r')
+        }else if(result[i].length===4){
+          frase.push('s')
+        }
+      }else if(result[i].startsWith(8)){
+        if(result[i].length===1){
+          frase.push('t')
+        }else if(result[i].length===2){
+          frase.push('u')
+        }else if(result[i].length===3){
+          frase.push('v')
+        }
+      }else if(result[i].startsWith(9)){
+        if(result[i].length===1){
+          frase.push('w')
+        }else if(result[i].length===2){
+          frase.push('x')
+        }else if(result[i].length===3){
+          frase.push('y')
+        }else if(result[i].length===3){
+          frase.push('z')
+        }
+      }
+    }
+}
+//
+frase = frase.join('')
+console.log({ myMessage: contagem,result,frase })
+return res.json(frase)
+})
+
 
 app.listen(3333, ()=>{
     console.log('server running...');
